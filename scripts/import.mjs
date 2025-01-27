0da6518126e46311e86142cc382b84cc33c035da
Andrea Minato <andreaminato@outlook.com>
2025-01-27 16:14:13 +0100

    generate from docs links

---
+++ scripts/import.mjs
@@ -0,0 +1,74 @@
+import { load } from 'cheerio'
+import { mkdir, appendFile, writeFile } from 'fs/promises'
+import { writeChapters } from './readChapters.mjs'
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function extractDocumentId(url) {
+  const regex = /\/d\/([^\/]+)/;
+  const match = url.match(regex);
+  return match ? match[1] : null; // Returns the ID or null if not found
+}
+
+function extractIdsFromHTML(htmlString) {
+  const $ = load(htmlString); // Load the HTML string into cheerio
+  const ids = [];
+
+  // Loop through all <a> elements and extract their href attributes
+  $('a').each((i, el) => {
+    const href = $(el).attr('href');
+    if (href) {
+      const id = extractDocumentId(href)
+      if (id) {
+        ids.push(id)
+      }
+    }
+  });
+
+  return ids;
+}
+
+async function loadGoogleDoc(id, format = 'txt') {
+  const url = `https://docs.google.com/document/d/${id}/export?format=${format}`;
+  const response = await fetch(url)
+
+  if (!response.ok) {
+    console.log(response)
+    throw new Error("Network response was not ok");
+  }
+  const text = await response.text();
+  return text
+}
+
+async function loadNovel(_id, novel) {
+
+  await mkdir('./tmp/' + novel, { recursive: true })
+  await writeFile('./tmp/' + novel + '/chapters.md', '')
+
+  const content = await loadGoogleDoc(_id, 'html')
+
+  const ids = extractIdsFromHTML(content)
+
+  for (const id of ids) {
+    try {
+      const text = await loadGoogleDoc(id)
+      appendFile('./tmp/' + novel + '/chapters.md', '\n\n' + text)
+    } catch (error) {
+
+    }
+  }
+
+  await sleep(500)
+  writeChapters('./tmp/' + novel + '/chapters.md', novel)
+}
+
+
+// loadNovel('1p_XRL5cg2KaBDZpC2YSkKT1TsO8gUdvj2HZDskB2rOg', 'mga')
+// loadNovel('1UAr63ltyIGu9a8cbxL7nziRYzJeW2X_x96oWyEMxhbA, 'atg')
+// loadNovel('1c3IGtRohe6IklxlFy2Cn0Ts__WbQQBX-ikNJ7wCZx30', 'htk')
+// loadNovel('1AKE2CdyIllmsBW3ItSwlE7E9VjQMRYduZFla_vY5mPU', 'tmw')
+// loadNovel('1NlmUC5zJDSA1GeOP-zXzeOiSD8LGufki2AIB5equGQE', 'lrg')
+// loadNovel('1i2opAYNXvXzMrPJI5E4b-8xNtx1vj0iL3lRBFbD-sgk', 'cd')
+// loadNovel('17m97EysE3iS2x1ufHCUZHBBXMzpmqyzIo7erIp60Z6A', 'mw')
---
+++ scripts/readChapters.mjs
@@ -0,0 +1,89 @@
+import { writeFileSync, createReadStream, existsSync } from 'fs'
+import { createInterface } from 'readline'
+
+const base = `---
+title: [TITLE]
+pubDate: [DATE]
+tags:
+    - [TAG]
+---
+[CONTENT]
+`
+
+
+const regx = /Capitolo [0-9]/g
+
+
+function createFile({
+  title,
+  content,
+  tag
+}) {
+  let number = title.match(/[0-9]+/)[0]
+
+  const difference = -6000 + parseInt(number)
+  const date = Date.now() + difference * 60 * 60 * 1000
+
+  if (title.includes('prima')) {
+    number += '_a'
+  }
+  if (title.includes('seconda')) {
+    number += '_b'
+  }
+  if (title.includes('terza')) {
+    number += '_c'
+  }
+  const path = './src/content/novels/' + tag + '/' + number + '.md'
+
+  if (existsSync(path)) {
+    console.log(`Skip ${path}`);
+    return;
+  }
+
+  console.log(`Creating ${path}`)
+
+  writeFileSync(
+    path,
+    base
+      .replace('[TAG]', tag)
+      .replace('[TITLE]', title)
+      .replace('[CONTENT]', content)
+      .replace('[DATE]', new Date(date).toISOString())
+  )
+}
+
+export function writeChapters(path, TAG) {
+  const rl = createInterface({
+    input: createReadStream(path),
+    output: undefined,
+    console: false
+  })
+
+  let title = ''
+  let content = ''
+
+  rl.on('line', function (line) {
+    if (regx.test(line)) {
+      if (title) {
+        createFile({
+          title: title,
+          content: content,
+          tag: TAG
+        })
+      }
+      title = line
+      content = ''
+    } else {
+      content += line + '\n'
+    }
+  });
+
+  rl.on('close', function () {
+    createFile({
+      title: title,
+      content: content,
+      tag: TAG
+    })
+  })
+
+}
