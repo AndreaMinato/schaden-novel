8b8a9ebb6bc6e324a751666cde19ab84aefc78c2
Andrea Minato <andreaminato@outlook.com>
2026-06-22 18:03:38 +0200

    Update import.mjs

--- scripts/import.mjs
+++ scripts/import.mjs
@@ -2,16 +2,16 @@ import { loadGoogleDoc, extractDocumentId } from './shared.mjs'
 import { loadNovel } from './loadNovel.mjs'
 
 const NOVELS = {
-  atg: '1UAr63ltyIGu9a8cbxL7nziRYzJeW2X_x96oWyEMxhbA',
+  // atg: '1UAr63ltyIGu9a8cbxL7nziRYzJeW2X_x96oWyEMxhbA',
   // cd: '1i2opAYNXvXzMrPJI5E4b-8xNtx1vj0iL3lRBFbD-sgk',
   htk: '1c3IGtRohe6IklxlFy2Cn0Ts__WbQQBX-ikNJ7wCZx30',
-  issth: '1XNSlUXLISdDebkLiWmRx90Utc5MfJjbxk0qezNwlqHM',
+  // issth: '1XNSlUXLISdDebkLiWmRx90Utc5MfJjbxk0qezNwlqHM',
   // lrg: '1NlmUC5zJDSA1GeOP-zXzeOiSD8LGufki2AIB5equGQE',
-  mga: '1p_XRL5cg2KaBDZpC2YSkKT1TsO8gUdvj2HZDskB2rOg',
-  mw: '17m97EysE3iS2x1ufHCUZHBBXMzpmqyzIo7erIp60Z6A',
+  // mga: '1p_XRL5cg2KaBDZpC2YSkKT1TsO8gUdvj2HZDskB2rOg',
+  // mw: '17m97EysE3iS2x1ufHCUZHBBXMzpmqyzIo7erIp60Z6A',
   overgeared: '1ltYlFG6qnH-rT8-aPtbCJeGZepsR_AX8x2mK9ieVGng',
-  rtw: '1UlpiIFhcvkDo_yB9YKpgubhRmxeSFxZKtcflD2Sunok',
-  tmw: '1AKE2CdyIllmsBW3ItSwlE7E9VjQMRYduZFla_vY5mPU',
+  // rtw: '1UlpiIFhcvkDo_yB9YKpgubhRmxeSFxZKtcflD2Sunok',
+  // tmw: '1AKE2CdyIllmsBW3ItSwlE7E9VjQMRYduZFla_vY5mPU',
 }
 
 function extractIdsFromHTML(htmlString) {
